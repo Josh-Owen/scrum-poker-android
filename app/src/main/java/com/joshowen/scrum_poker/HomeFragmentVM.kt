29340787ed19c9cceb33b2632f5471e88d4b0697
Josh Owen <erial909@hotmail.com>
2022-04-06 11:24:54 +0100

    Initial commit - project setup
    Installed core dependancies and implemented base classes and added a navgraph.

---
+++ app/src/main/java/com/joshowen/scrum_poker/HomeFragmentVM.kt
@@ -0,0 +1,24 @@
+package com.joshowen.scrum_poker
+
+interface Input {
+
+}
+
+interface Output {
+
+}
+
+class HomeFragmentVM : Input, Output {
+
+    init {
+
+    }
+
+    //region Inputs
+
+    //endregion
+
+    //region Output
+
+    //endregion
+}
---
+++ app/src/main/java/com/joshowen/scrum_poker/base/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.joshowen.scrum_poker.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
