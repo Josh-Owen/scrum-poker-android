00beee84e991c6aec6a1da14f08238f04513f93d
Josh Owen <erial909@hotmail.com>
2022-04-27 04:03:13 +0100

    Updated CardData.kt the app default values

--- app/src/main/java/com/joshowen/scrum_poker/types/datatypes/CardData.kt
+++ app/src/main/java/com/joshowen/scrum_poker/types/datatypes/CardData.kt
@@ -3,4 +3,4 @@ package com.joshowen.scrum_poker.types.datatypes
 import com.joshowen.scrum_poker.R
 import com.joshowen.scrum_poker.types.enums.CardType
 
-data class CardData(val cardType : CardType, val value : String = "", val backgroundColourResourceId : Int = R.color.card_colour_blue, val cardContentResourceId : Int= R.color.white, val resourceId : Int? = null)
+data class CardData(val cardType : CardType, val value : String = "", val backgroundColourResourceId : Int = R.color.card_item_background_default, val cardContentResourceId : Int= R.color.card_item_content_colour_default, val resourceId : Int? = null)
